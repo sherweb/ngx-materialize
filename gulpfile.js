de71854457857a4baefaee2bc1d2888c1248bbff
Nicolas Charpentier <ncharpentier@sherweb.com>
2017-01-16 14:48:42 -0500

    Add task to transpile source

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var inlineNg2Template = require('gulp-inline-ng2-template');
+
+gulp.task('default', function () {
+  return gulp.src(['./src/app/**/*.ts', '!./src/app/**/*.spec.ts'])
+    .pipe(inlineNg2Template({
+      base: '/src/app',
+      useRelativePaths: true,
+    }))
+    .pipe(gulp.dest('./inline-template'))
+});
