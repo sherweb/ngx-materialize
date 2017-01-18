34f35dfaf25a03741e54b2e2d29248078751563f
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2017-01-18 11:53:09 -0500

    Add task to transpile source (#2)
    * Add task to transpile source
    * Fix linter issues

--- src/app/input/input-container/input-container.component.view.spec.ts
+++ src/app/input/input-container/input-container.component.view.spec.ts
@@ -1,6 +1,6 @@
 import { async, TestBed } from '@angular/core/testing';
 
-import { MzTestWrapperComponent, buildComponent } from '../../shared/test-wrapper';
+import { buildComponent, MzTestWrapperComponent } from '../../shared/test-wrapper';
 import { MzInputContainerComponent } from './input-container.component';
 
 describe('MzInputContainerComponent:view', () => {
