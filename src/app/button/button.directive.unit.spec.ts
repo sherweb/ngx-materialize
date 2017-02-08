0c8107de8d9e435b44eed7f52fc294cc410483c9
Sébastien Côté <scote@sherweb.com>
2017-02-08 16:28:21 -0500

    Add missing unit tests for button directive
    - Remove button component unit tests.

--- src/app/button/button.directive.unit.spec.ts
+++ src/app/button/button.directive.unit.spec.ts
@@ -80,5 +80,126 @@ describe('MzButtonDirective:unit', () => {
         expect(directive[handler]).toHaveBeenCalled();
       });
     });
+
+    describe('handleDisabled', () => {
+
+      it('should add disabled css class on button element when disabled is true', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.disabled = true;
+        directive.handleDisabled();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'disabled', directive.disabled);
+      });
+
+      it('should not add disabled css class on button element when disabled is false', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.disabled = false;
+        directive.handleDisabled();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'disabled', directive.disabled);
+      });
+    });
+
+    describe('handleFlat', () => {
+
+      it('should add btn-flat css class on button element when flat is true', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.flat = true;
+        directive.handleFlat();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-flat', directive.flat);
+      });
+
+      it('should not add btn-flat css class on button element when flat is false', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.flat = false;
+        directive.handleFlat();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-flat', directive.flat);
+      });
+    });
+
+    describe('handleFloat', () => {
+
+      it('should add btn-floating css class on button element when float is true', () => {
+        spyOn(renderer, 'setElementClass');
+
+        directive.float = true;
+        directive.handleFloat();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-floating', directive.float);
+      });
+
+      it('should not add btn-floating css class on button element when float is false', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.float = false;
+        directive.handleFloat();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-floating', directive.float);
+      });
+    });
+
+    describe('handleLarge', () => {
+
+      it('should add btn-large css class on button element when large is true', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.large = true;
+        directive.handleLarge();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-large', directive.large);
+      });
+
+      it('should not add btn-large css class on button element when large is false', () => {
+
+        spyOn(renderer, 'setElementClass');
+
+        directive.large = false;
+        directive.handleLarge();
+
+        expect(renderer.setElementClass).toHaveBeenCalledWith(mockElementRef.nativeElement, 'btn-large', directive.large);
+      });
+    });
+
+    describe('handleNoWaves', () => {
+      it('should add waves-effect and waves-light css class on button element when noWaves is false', () => {
+
+        const rendererSpy = spyOn(renderer, 'setElementClass');
+
+        directive.noWaves = false;
+        directive.handleNoWaves();
+
+        expect(rendererSpy.calls.count()).toBe(2);
+        expect(rendererSpy.calls.allArgs()).toEqual([
+          [mockElementRef.nativeElement, 'waves-effect', !directive.noWaves],
+          [mockElementRef.nativeElement, 'waves-light', !directive.noWaves],
+        ]);
+      });
+
+      it('should not add waves-effect and waves-light on button element when noWaves is true', () => {
+
+        const rendererSpy = spyOn(renderer, 'setElementClass');
+
+        directive.noWaves = true;
+        directive.handleNoWaves();
+
+        expect(rendererSpy.calls.count()).toBe(2);
+        expect(rendererSpy.calls.allArgs()).toEqual([
+          [mockElementRef.nativeElement, 'waves-effect', !directive.noWaves],
+          [mockElementRef.nativeElement, 'waves-light', !directive.noWaves],
+        ]);
+      });
+    });
   });
 });
