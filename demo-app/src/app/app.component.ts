8389b68bdffc59a35f0159ca87682a3ba43dcf70
Martin Roy <mrmartinroy@gmail.com>
2017-02-08 15:11:39 -0500

    Goup menu link by section (#30)
    * Goup menu link by section
    * Rework sections for easier understanding

--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -3,19 +3,43 @@ import 'rxjs/add/operator/filter';
 import { Component, ElementRef, OnInit, Renderer } from '@angular/core';
 import { NavigationEnd, Route, Router } from '@angular/router';
 
+type SectionRoutesPair = {section: string, routes: Route[]};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  public routes: Route[];
+  public groupedRoutes: Array<SectionRoutesPair>;
 
   constructor(private router: Router, private renderer: Renderer, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
-    this.routes = this.router.config;
+    this.populateSideNavWithRoutesGroupedBySections();
+    this.setNavigationEndEvent();
+  }
 
+  populateSideNavWithRoutesGroupedBySections() {
+    // Take all routes with data and group them by sections
+    this.groupedRoutes = this.router.config.reduce<Array<SectionRoutesPair>>(
+      (returnValues, currentValue: any) => { // currentValue is a route. But, if typed, doesn't compile because of the 'data.section' access
+        if (currentValue.data) {
+          const section = currentValue.data.section;
+          const existingSection = returnValues.find((r) => r && r.section === section);
+
+          if (existingSection) {
+            existingSection.routes.push(currentValue);
+          } else {
+            returnValues.push({ section: section, routes: [currentValue] });
+          }
+        }
+        return returnValues;
+      },
+      new Array<SectionRoutesPair>());
+  }
+
+  setNavigationEndEvent() {
     // scroll to top on each route change
     this.router.events
       .filter((event) => event instanceof NavigationEnd)
