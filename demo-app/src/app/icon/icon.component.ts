949a4cbd5607cf096157c226086fc3907bf77d23
Sébastien Côté <scote@sherweb.com>
2017-02-09 11:01:10 -0500

    Remove variable used in first icon demo

--- demo-app/src/app/icon/icon.component.ts
+++ demo-app/src/app/icon/icon.component.ts
@@ -9,7 +9,4 @@ import { ROUTE_ANIMATION, ROUTE_ANIMATION_HOST } from '../app.routes.animation';
   host: ROUTE_ANIMATION_HOST, // tslint:disable-line:use-host-property-decorator
   animations: [ROUTE_ANIMATION],
 })
-export class IconComponent {
-
-  icon: string  = 'cloud';
-}
+export class IconComponent { }
