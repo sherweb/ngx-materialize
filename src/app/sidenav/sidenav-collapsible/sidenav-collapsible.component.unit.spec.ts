4b5e242123e53564764259ea3d892c56a38e39c4
Sébastien Côté <scote@sherweb.com>
2017-04-10 14:35:28 -0400

    Add onClose and onOpen callback to sidenav collapsible

--- src/app/sidenav/sidenav-collapsible/sidenav-collapsible.component.unit.spec.ts
+++ src/app/sidenav/sidenav-collapsible/sidenav-collapsible.component.unit.spec.ts
@@ -1,5 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { MzSidenavCollapsibleComponent } from './sidenav-collapsible.component';
 
@@ -21,7 +21,60 @@ describe('MzSidenavCollapsibleComponent:unit', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('ngAfterViewInit', () => {
+
+    it('should call initCollapsible function', () => {
+      spyOn(component, 'initCollapsible');
+
+      component.initCollapsible();
+
+      expect(component.initCollapsible).toHaveBeenCalled();
+    });
+  });
+
+  describe('initCollapsible', () => {
+
+    function forceSetTimeoutEnd() {
+      tick(1); // force setTimeout execution
+    }
+
+    it('should initialize collapsible using jquery', fakeAsync(() => {
+
+      component.onClose = () => { const close = 'close'; };
+      component.onOpen = () => { const open = 'open'; };
+
+      const mockJQueryCollapsibleNativeElement = { collapsible: true };
+
+      spyOn(component.renderer, 'invokeElementMethod');
+
+      spyOn(window, '$').and.callFake((selector: any) => {
+        return selector === component.collapsible.nativeElement
+          ? mockJQueryCollapsibleNativeElement
+          : {};
+      });
+
+      component.initCollapsible();
+
+      forceSetTimeoutEnd();
+
+      expect(component.renderer.invokeElementMethod)
+        .toHaveBeenCalledWith(
+          mockJQueryCollapsibleNativeElement,
+          'collapsible', [{
+            accordion: false,
+            onClose: component.onClose,
+            onOpen: component.onOpen,
+          }],
+        );
+    }));
+
+    it('should call detectChanges', () => {
+
+      spyOn(component.changeDetectorRef, 'detectChanges').and.callThrough();
+
+      component.initCollapsible();
+
+      expect(component.changeDetectorRef.detectChanges).toHaveBeenCalled();
+    });
   });
 });
