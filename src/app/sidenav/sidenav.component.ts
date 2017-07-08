c4defaa32c94e072a873732e54102558133e2f9c
Florian Merz <florianmrz@users.noreply.github.com>
2017-07-08 22:27:43 +0200

    Removed unneccessary styling of side-nav component

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -10,8 +10,7 @@ import {
 
 @Component({
   selector: 'mz-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
+  templateUrl: './sidenav.component.html'
 })
 export class MzSidenavComponent implements AfterViewInit, OnDestroy, OnInit {
   @Input() backgroundClass: string;
