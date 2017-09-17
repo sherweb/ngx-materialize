d35534d3d383a352835fc37dbf236dffaaa8d07b
jfcere <jfcere@sherweb.com>
2017-09-17 16:59:12 -0400

    Add Switch tests

--- src/app/switch/switch-container/switch-container.component.view.spec.ts
+++ src/app/switch/switch-container/switch-container.component.view.spec.ts
@@ -1,25 +1,90 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
+import { buildComponent, MzTestWrapperComponent } from '../../shared/test-wrapper';
+import { MzSwitchDirective } from '../switch.directive';
 import { MzSwitchContainerComponent } from './switch-container.component';
 
 describe('MzSwitchContainerComponent:view', () => {
-  let component: MzSwitchContainerComponent;
-  let fixture: ComponentFixture<MzSwitchContainerComponent>;
+  let nativeElement: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MzSwitchContainerComponent],
-    })
-    .compileComponents();
+      declarations: [
+        MzSwitchDirective,
+        MzSwitchContainerComponent,
+        MzTestWrapperComponent,
+      ],
+    });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MzSwitchContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  describe('switch', () => {
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+    function lever(): HTMLElement {
+      return nativeElement.querySelector('.switch label span.lever');
+    }
+
+    function off(): HTMLElement {
+      return nativeElement.querySelector('.switch label span.off');
+    }
+
+    function on(): HTMLElement {
+      return nativeElement.querySelector('.switch label span.on');
+    }
+
+    function switchContainer(): HTMLElement {
+      return nativeElement.querySelector('.switch');
+    }
+
+    function switchInput(): HTMLElement {
+      return nativeElement.querySelector('.switch label input[mz-switch]');
+    }
+
+    it('should transclude correctly', async(() => {
+
+      buildComponent(`
+        <mz-switch-container>
+          switch-content-x
+        </mz-switch-container>
+      `).then((fixture) => {
+        nativeElement = fixture.nativeElement;
+        fixture.detectChanges();
+
+        expect(switchContainer().innerText).toBe('switch-content-x');
+      });
+    }));
+
+    it('should have on/off labels shown correctly when provided', async(() => {
+
+      buildComponent(`
+        <mz-switch-container>
+          <input mz-switch [off]="'off-x'" [on]="'on-x'">
+        </mz-switch-container>
+      `).then((fixture) => {
+        nativeElement = fixture.nativeElement;
+        fixture.detectChanges();
+
+        expect(off().innerText).toBe('off-x');
+        expect(switchInput()).toBeTruthy();
+        expect(lever()).toBeTruthy();
+        expect(on().innerText).toBe('on-x');
+      });
+    }));
+
+    it('should have on/off labels shown correctly when not provided', async(() => {
+
+      buildComponent(`
+        <mz-switch-container>
+          <input mz-switch>
+        </mz-switch-container>
+      `).then((fixture) => {
+        nativeElement = fixture.nativeElement;
+        fixture.detectChanges();
+
+        expect(off().innerText).toBe('');
+        expect(switchInput()).toBeTruthy();
+        expect(lever()).toBeTruthy();
+        expect(on().innerText).toBe('');
+      });
+    }));
   });
 });
