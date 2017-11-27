511a0a3095a975ffd7c7b4bcd38a6640d0b6313b
jfcere <jfcere@sherweb.com>
2017-11-26 22:33:36 -0500

    Fix export of MzTimepickerModule and remove unused imports

--- src/app/checkbox/checkbox.module.ts
+++ src/app/checkbox/checkbox.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MzCheckboxContainerComponent } from './checkbox-container';
--- src/app/radio-button/radio-button.module.ts
+++ src/app/radio-button/radio-button.module.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MzRadioButtonContainerComponent } from './radio-button-container';
