15f3afd2bb495dd6f6a7f855d7cbce320c995953
Sebastien Cote <sebastien.r.cote@gmail.com>
2018-04-08 21:13:46 -0400

    Add open/close programmatically for collapsible

--- src/app/collapsible/collapsible.component.ts
+++ src/app/collapsible/collapsible.component.ts
@@ -4,9 +4,11 @@ import {
   ContentChildren,
   ElementRef,
   Input,
+  OnDestroy,
   QueryList,
   Renderer,
-  ViewChild } from '@angular/core';
+  ViewChild,
+} from '@angular/core';
 
 import { MzCollapsibleItemComponent } from './collapsible-item/collapsible-item.component';
 
@@ -15,7 +17,7 @@ import { MzCollapsibleItemComponent } from './collapsible-item/collapsible-item.
   templateUrl: './collapsible.component.html',
   styleUrls: ['./collapsible.component.scss'],
 })
-export class MzCollapsibleComponent implements AfterViewInit {
+export class MzCollapsibleComponent implements AfterViewInit, OnDestroy {
   @Input() mode: string;
   @Input() onClose: Function;
   @Input() onOpen: Function;
@@ -33,6 +35,14 @@ export class MzCollapsibleComponent implements AfterViewInit {
     this.initCollapsible();
   }
 
+  ngOnDestroy(): void {
+    $(this.collapsible.nativeElement).collapsible('destroy');
+  }
+
+  close(collapsibleItemIndex: number) {
+    $(this.collapsible.nativeElement).collapsible('close', collapsibleItemIndex);
+  }
+
   initCollapsible() {
     const options: Materialize.CollapsibleOptions = {
       accordion: false,
@@ -40,12 +50,16 @@ export class MzCollapsibleComponent implements AfterViewInit {
       onOpen: this.onOpen,
     };
 
-    this.renderer.invokeElementMethod($(this.collapsible.nativeElement), 'collapsible', [options]);
+    $(this.collapsible.nativeElement).collapsible(options);
   }
 
   handleDataCollapsible() {
     if (this.mode) {
       this.renderer.setElementAttribute(this.collapsible.nativeElement, 'data-collapsible', this.mode);
     }
+  }
+
+  open(collapsibleItemIndex: number) {
+    $(this.collapsible.nativeElement).collapsible('open', collapsibleItemIndex);
   }
 }
