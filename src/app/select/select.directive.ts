2be6ac7d34a60d6235d657af110f44c0c0d475b3
Samuel Bergeron <sabergeron@sherweb.com>
2018-05-11 08:29:38 -0400

    #95319: Realigned the code in select component

--- src/app/select/select.directive.ts
+++ src/app/select/select.directive.ts
@@ -130,7 +130,7 @@ export class MzSelectDirective extends HandlePropChanges implements OnInit, OnDe
   handleDOMEvents() {
     this.inputElement.on('blur focus', (event: JQuery.Event) => {
       const customEvent = document.createEvent('CustomEvent');
-        customEvent.initCustomEvent(event.type, true, false, event.target);
+      customEvent.initCustomEvent(event.type, true, false, event.target);
       this.selectElement[0].dispatchEvent(customEvent);
     });
   }
