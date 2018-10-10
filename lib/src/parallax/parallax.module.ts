5fd04190fb7c0d782b93d4811c62c7dd06fc11f7
Tobias Wittwer <t.wittwer95@gmail.com>
2018-10-10 22:22:08 +0200

    Add option for content on parallax image
    #251

--- lib/src/parallax/parallax.module.ts
+++ lib/src/parallax/parallax.module.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MzParallaxComponent } from './parallax.component';
 
 @NgModule({
+  imports: [
+    CommonModule,
+  ],
   declarations: [MzParallaxComponent],
   exports: [MzParallaxComponent],
 })
